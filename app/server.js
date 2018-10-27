934bd06005422ea9354a73d5b5fcd71c7cb1ad3e
LaurenSantos23 <Lauren.Santosuosso@yahoo.com>
2018-10-27 18:25:41 -0400

    trying to set up express port

--- app/server.js
+++ app/server.js
@@ -6,11 +6,22 @@ const path = require('path');
 //Setting up express App
 
 const app = express();
-const PORT = 3000;
+
+//Setting port to 3000
+const PORT = process.env.PORT || 3000;
 
 // Sets up Express app to handle data parsing
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-console.log
+//Routes 
+
+//Basic route that sends the user first to the Welcome Page 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.listen(PORT, function() {
+    console.log('App listening on: http://localhost:' + PORT);
+});
 
