7383a71c7d16128b177eef4d679f931a64c2b2da
LaurenSantos23 <Lauren.Santosuosso@yahoo.com>
2018-10-27 22:52:49 -0400

    added a ; line 28

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -25,7 +25,7 @@ app.post("/api/friends", function(req, res){
     for (const i=0; i<friends.length; i++){
         const totalDifference = 0;
         //run through the scores in the friends array to compare friends
-        for (const f=0; f<newFriendScore.length, f++) {
+        for (const f=0; f<newFriendScore.length, f++;) {
             totalDifference +- (Math.abs(parseInt(friends[i].scores[j])- parseInt(newFriendScore[j])));
         }
         //push into the scoresArray
