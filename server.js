3c728822eb9c47e09dbca4b8ce465258707b860b
LaurenSantos23 <Lauren.Santosuosso@yahoo.com>
2018-10-28 17:42:28 -0400

    removed a line of psuedo code

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-// there is an error being thrown about apiRoutes when I run node.js
+
  require('./app/routing/apiRoutes')(app)
  require("./app/routing/htmlRoutes")(app)
 
