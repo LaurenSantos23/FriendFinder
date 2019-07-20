8b9e9b48d6168306da993b0da2b59167c668a1f2
Lauren Santosuosso <Lauren.Santosuosso@yahoo.com>
2019-07-20 14:03:02 -0400

    .

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 
- require('./app/routing/apiRoutes')(app)
- require("./app/routing/htmlRoutes")(app)
+ require('./app/routing/apiRoutes')(app);
+ require("./app/routing/htmlRoutes")(app);
 
 
 // Code that starts the server to begin listening
